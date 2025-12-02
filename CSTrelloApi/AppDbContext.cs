90df0267e132ec04beeab0dceb219b2b90e2e269
Royboot <rgrizell467@gmail.com>
2025-12-02 15:58:39 +0100

    added Tasks to appDbContext

---
+++ CSTrelloApi/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CSTrelloApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSTrelloApi;
+
+public class AppDbContext:DbContext
+{
+    public DbSet<Tasks> Tasks { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("server=localhost;port=3306;database=Vehicles;user=root;password=;",ServerVersion.Parse("8.0.30"));
+    }
+}
