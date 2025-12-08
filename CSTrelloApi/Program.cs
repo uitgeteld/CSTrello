c09f0f70548d7f43a18b895e5302394b90011314
Royboot <rgrizell467@gmail.com>
2025-12-08 12:17:02 +0100

    added a /all, register and login route to api

--- CSTrelloApi/Program.cs
+++ CSTrelloApi/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
+using System.Reflection.Metadata;
 using System.Text.Json;
 using CSTrelloApi.Data;
+using DevOne.Security.Cryptography.BCrypt;
 using Task = CSTrelloApi.Data.Task;
 
 namespace CSTrelloApi
@@ -30,13 +32,50 @@ namespace CSTrelloApi
                 using (var db = new AppDbContext())
                 {
                     var tasks = db.Tasks.ToList();
-                    responseString = "<html><body><h1>All Cars</h1><ul>";
+                    responseString = "<html><body><h1>All Tasks</h1><ul>";
                     foreach (var task in tasks)
                     {
                         responseString += $"<li>{task.Id} {task.Title} ({task.AssignedTo}) - {task.Description} - {task.Status}</li>";
                     }
                     responseString += "</ul></body></html>";
                 }
+            }else if(request.Url.AbsolutePath == "Edit" && request.HttpMethod == "POST"){
+                
+                using (var db = new AppDbContext())
+                {
+                    var form = request.InputStream;
+                    var reader = new System.IO.StreamReader(form);
+                    var body = reader.ReadToEnd();
+                    var task = JsonSerializer.Deserialize<Task>(body);
+                    if (task != null)
+                    {
+                        Task taskFromDb = db.Tasks.FirstOrDefault(t => t.Id == task.Id);
+                        if (taskFromDb != null)
+                        {
+                            taskFromDb.Title = task.Title;
+                            taskFromDb.Description = task.Description;
+                            taskFromDb.Status = task.Status;
+                            taskFromDb.AssignedTo = task.AssignedTo;
+                            db.Tasks.Update(taskFromDb);
+                            db.SaveChanges();
+                            response.StatusCode = 201;
+                            return;
+                        }
+                        else
+                        {
+                            response.StatusCode = 404;
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        response.StatusCode = 404;
+                        return;
+                    }
+
+                }
+                
             }else if (request.Url.AbsolutePath == "/add"&& request.HttpMethod == "POST")
             {
                 using (var db = new AppDbContext())
@@ -48,10 +87,11 @@ namespace CSTrelloApi
                     db.Tasks.Add(task);
                     db.SaveChanges();
                     response.StatusCode = 201;
+                    return;
                 }
                 
 
-            }else if (request.Url.AbsolutePath == "/remove")
+            }else if (request.Url.AbsolutePath == "/remove" && request.HttpMethod == "DELETE")
             {
                 using (var db = new AppDbContext())
                 {
@@ -69,17 +109,73 @@ namespace CSTrelloApi
                     {
                         db.Tasks.Remove(taskToRemove);
                         db.SaveChanges();
-                        response.StatusCode = 200;  
+                        response.StatusCode = 200;
+                        return;
                     }
                     else
                     {
                         response.StatusCode = 404;
+                        return;
                     }
                 }
-            }else
+            }else if ( request.Url.AbsolutePath == "/users")
+            {
+                using(var db = new AppDbContext())
+                {
+                    var users = db.Users.ToList();
+                    responseString = "<html><body><h1>All Users</h1><ul>";
+                    foreach (var user in users)
+                    {
+                        responseString += $"<li>{user.Name}</li>";
+                    }
+                    responseString += "</ul></body></html>";
+                }
+                
+            } else if (request.Url.AbsolutePath == "/register" && request.HttpMethod == "POST")
+            {
+                using (var db = new AppDbContext())
+                {
+                    var form = request.InputStream;
+                    var reader = new System.IO.StreamReader(form);
+                    var body = reader.ReadToEnd();
+                    var user = JsonSerializer.Deserialize<User>(body);
+                    if (user == null)
+                    {
+                        response.StatusCode = 404;
+                        return;
+                    }
+                    user.Password = BCryptHelper.HashPassword(user.Password, BCryptHelper.GenerateSalt());
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    response.StatusCode = 201;
+                }
+            }else if (request.Url.AbsolutePath == "/login" && request.HttpMethod == "POST")
+            {
+                using (var db = new AppDbContext())
+                {
+                    var form = request.InputStream;
+                    var reader = new System.IO.StreamReader(form);
+                    var body = reader.ReadToEnd();
+                    var user = JsonSerializer.Deserialize<User>(body);
+                    if (user == null)
+                    {
+                        response.StatusCode = 404;
+                        return;
+                    }
+                    bool isPasswordValid = false;
+                    var userFromDb = db.Users.FirstOrDefault(u => u.Id == user.Id);
+                    if (userFromDb != null)
+                    {
+                        isPasswordValid = BCryptHelper.CheckPassword(user.Password, userFromDb.Password);
+                        response.StatusCode = isPasswordValid ? 201 : 404;
+                        return;
+                    }
+                }
+            } else
             {
                 responseString = "<html><body>404 Not Found</body></html>";
                 response.StatusCode = 404;
+                return;
             }
             byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
             response.ContentLength64 = buffer.Length;
