7ee8967095a70ce4122cc94321913969c941eecf
uitgeteld <66201378+uitgeteld@users.noreply.github.com>
2026-01-15 01:13:37 +0100

    Added attributes

--- CSTrelloApp/Data/Task.cs
+++ CSTrelloApp/Data/Task.cs
@@ -1,14 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace CSTrelloApp.Data;
+using System.ComponentModel.DataAnnotations;
 
-namespace CSTrelloApp.Data;
 public class Task
 {
     public int Id { get; set; }
 
     [Required]
+    [StringLength(100)]
     public string Title { get; set; }
-    public string Description { get; set; }
-    public string Status { get; set; }
-    public string AssignedTo  { get; set; }
-}
 
+    [StringLength(500)]
+    public string Description { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string Status { get; set; }
+
+    [StringLength(100)]
+    public string AssignedTo { get; set; }
+}
--- CSTrelloApp/Data/User.cs
+++ CSTrelloApp/Data/User.cs
@@ -1,9 +1,20 @@
 ﻿namespace CSTrelloApp.Data;
+using System.ComponentModel.DataAnnotations;
 
 public class User
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(255)]
     public string Email { get; set; }
+
+    [Required]
+    [StringLength(255)]
     public string Password { get; set; }
 }
